bb5596920176bc6c68280395a73e85d3dc02a16c
Mathura <Torque-Lab@users.noreply.github.com>
2025-03-30 14:29:04 +0530

    added signup end point

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,21 +3,34 @@ import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { createUserSchema } from "@repo/common/types";
-import 
+import { prismaClient } from "@repo/db/client";
+
 const app = express();
 console.log(JWT_SECRET);
 //@ts-ignore
-app.post("/signup", (req, res) => {
-  const data = createUserSchema.safeParse(req.body);
-  if (!data.success) {
+app.post("/signup", async (req, res) => {
+  const parsedData = createUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
     return res.json({
       message: "Invalid input",
     });
+    return;
   }
+
   //db call
-  res.json({
-    roomId: 123,
-  });
+  try {
+    await prismaClient.user.create({
+      data: {
+        email: parsedData.data?.username,
+        password: parsedData.data?.password,
+        name: parsedData.data.name,
+      },
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
 });
 
 //@ts-ignore
